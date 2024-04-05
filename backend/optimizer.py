09fd0ffdea818ed3a5056377e5c86f829e865761
Mann Dharmesh Acharya <89384052+mach-12@users.noreply.github.com>
2024-04-05 19:46:52 +0530

    Update optimizer.py

--- backend/optimizer.py
+++ backend/optimizer.py
@@ -45,31 +45,30 @@ def generate_portfolio(port_value, option):
 
     plots = {'kmeans_plot':None, 'portfolio_plot':None}
 
-    try:
-        # Define assets and index based on the selected option
-        if (option==1):
-          index='^NSEMDCP50'
-          assets = ['ABB.NS', 'AUBANK.NS', 'ABBOTINDIA.NS', 'ALKEM.NS', 'ASHOKLEY.NS', 'ASTRAL.NS', 'AUROPHARMA.NS', 'BALKRISIND.NS', 'BATAINDIA.NS', 'BHARATFORG.NS', 'CANBK.NS', 'COFORGE.NS', 'CONCOR.NS', 'CUMMINSIND.NS', 'ESCORTS.NS', 'FEDERALBNK.NS', 'GODREJPROP.NS', 'GUJGASLTD.NS', 'HINDPETRO.NS', 'HONAUT.NS', 'IDFCFIRSTB.NS', 'INDHOTEL.NS', 'JINDALSTEL.NS', 'JUBLFOOD.NS', 'LTTS.NS', 'LICHSGFIN.NS', 'LUPIN.NS', 'MRF.NS', 'M&MFIN.NS', 'MFSL.NS', 'OBEROIRLTY.NS', 'OFSS.NS', 'PAGEIND.NS', 'PERSISTENT.NS', 'PETRONET.NS', 'POLYCAB.NS', 'PFC.NS', 'PNB.NS', 'RECLTD.NS', 'SAIL.NS', 'TVSMOTOR.NS', 'TATACOMM.NS', 'TORNTPOWER.NS', 'TRENT.NS', 'UBL.NS', 'IDEA.NS', 'VOLTAS.NS', 'ZEEL.NS', 'ZYDUSLIFE.NS']
+    # Define assets and index based on the selected option
+    if (option==1):
+      index='^NSEMDCP50'
+      assets = ['ABB.NS', 'AUBANK.NS', 'ABBOTINDIA.NS', 'ALKEM.NS', 'ASHOKLEY.NS', 'ASTRAL.NS', 'AUROPHARMA.NS', 'BALKRISIND.NS', 'BATAINDIA.NS', 'BHARATFORG.NS', 'CANBK.NS', 'COFORGE.NS', 'CONCOR.NS', 'CUMMINSIND.NS', 'ESCORTS.NS', 'FEDERALBNK.NS', 'GODREJPROP.NS', 'GUJGASLTD.NS', 'HINDPETRO.NS', 'HONAUT.NS', 'IDFCFIRSTB.NS', 'INDHOTEL.NS', 'JINDALSTEL.NS', 'JUBLFOOD.NS', 'LTTS.NS', 'LICHSGFIN.NS', 'LUPIN.NS', 'MRF.NS', 'M&MFIN.NS', 'MFSL.NS', 'OBEROIRLTY.NS', 'OFSS.NS', 'PAGEIND.NS', 'PERSISTENT.NS', 'PETRONET.NS', 'POLYCAB.NS', 'PFC.NS', 'PNB.NS', 'RECLTD.NS', 'SAIL.NS', 'TVSMOTOR.NS', 'TATACOMM.NS', 'TORNTPOWER.NS', 'TRENT.NS', 'UBL.NS', 'IDEA.NS', 'VOLTAS.NS', 'ZEEL.NS', 'ZYDUSLIFE.NS']
+
+    if (option==2):
+      index='^NSEI'
+      assets = ['ADANIENT.NS', 'ADANIPORTS.NS', 'APOLLOHOSP.NS', 'ASIANPAINT.NS', 'AXISBANK.NS', 'BAJAJ-AUTO.NS', 'BAJFINANCE.NS', 'BAJAJFINSV.NS', 'BPCL.NS', 'BHARTIARTL.NS', 'BRITANNIA.NS', 'CIPLA.NS', 'COALINDIA.NS', 'DIVISLAB.NS', 'DRREDDY.NS', 'EICHERMOT.NS', 'GRASIM.NS', 'HCLTECH.NS', 'HDFCBANK.NS', 'HDFCLIFE.NS', 'HEROMOTOCO.NS', 'HINDALCO.NS', 'HINDUNILVR.NS', 'ICICIBANK.NS', 'ITC.NS', 'INDUSINDBK.NS', 'INFY.NS', 'JSWSTEEL.NS', 'KOTAKBANK.NS', 'LT.NS', 'M&M.NS', 'MARUTI.NS', 'NTPC.NS', 'NESTLEIND.NS', 'ONGC.NS', 'POWERGRID.NS', 'RELIANCE.NS', 'SBILIFE.NS', 'SBIN.NS', 'SUNPHARMA.NS', 'TCS.NS', 'TATACONSUM.NS', 'TATAMOTORS.NS', 'TATASTEEL.NS', 'TECHM.NS', 'TITAN.NS', 'UPL.NS', 'ULTRACEMCO.NS', 'WIPRO.NS']
+      
+    assets.append(index)
+
+    stockStartDate='2013-01-01'
+
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    # Download stock data using Yahoo Finance
+    df = yf.download(
+        tickers=assets,
+        start=stockStartDate,
+        end=today
+    )
     
-        if (option==2):
-          index='^NSEI'
-          assets = ['ADANIENT.NS', 'ADANIPORTS.NS', 'APOLLOHOSP.NS', 'ASIANPAINT.NS', 'AXISBANK.NS', 'BAJAJ-AUTO.NS', 'BAJFINANCE.NS', 'BAJAJFINSV.NS', 'BPCL.NS', 'BHARTIARTL.NS', 'BRITANNIA.NS', 'CIPLA.NS', 'COALINDIA.NS', 'DIVISLAB.NS', 'DRREDDY.NS', 'EICHERMOT.NS', 'GRASIM.NS', 'HCLTECH.NS', 'HDFCBANK.NS', 'HDFCLIFE.NS', 'HEROMOTOCO.NS', 'HINDALCO.NS', 'HINDUNILVR.NS', 'ICICIBANK.NS', 'ITC.NS', 'INDUSINDBK.NS', 'INFY.NS', 'JSWSTEEL.NS', 'KOTAKBANK.NS', 'LT.NS', 'M&M.NS', 'MARUTI.NS', 'NTPC.NS', 'NESTLEIND.NS', 'ONGC.NS', 'POWERGRID.NS', 'RELIANCE.NS', 'SBILIFE.NS', 'SBIN.NS', 'SUNPHARMA.NS', 'TCS.NS', 'TATACONSUM.NS', 'TATAMOTORS.NS', 'TATASTEEL.NS', 'TECHM.NS', 'TITAN.NS', 'UPL.NS', 'ULTRACEMCO.NS', 'WIPRO.NS']
-          
-        assets.append(index)
-    
-        stockStartDate='2013-01-01'
-    
-        today = datetime.today().strftime('%Y-%m-%d')
-    
-        # Download stock data using Yahoo Finance
-        df = yf.download(
-            tickers=assets,
-            start=stockStartDate,
-            end=today
-        )
-        
-    except:
-        raise Exception("Error in downloading stocks. Some stocks might be outdated/unavailabile. Feel free to rasie a PR to fix it.")
+    # except:
+    #     raise Exception("Error in downloading stocks. Some stocks might be outdated/unavailabile. Feel free to rasie a PR to fix it.")
         
     # Drop NaN values and focus on 'Adj Close' columns
     df = df.dropna(axis=1)
